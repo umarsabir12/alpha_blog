00de1dcc5ff31cf5a62d31264b1be9bf2740702d
umar sabir <umarsabir1997@gmail.com>
2020-11-03 13:59:30 +0500

    initial commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,30 @@
 class ArticlesController < ApplicationController
 
   def show
-
+   
     @article = Article.find(params[:id]) 
   end
-
+  
   def index
   @article = Article.all
   end
- 
-  def new
   
+  def new
+    @article = Article.new
   end 
-
+  
   def create
+  #  @article = Article.new(params.require(:article).permit(:title, :description))
+  #  @article.save
+  #  redirect_to @article
+
    @article = Article.new(params.require(:article).permit(:title, :description))
-   @article.save
-  #  render plain: @article.inspect 
+   if @article.save
+     flash[:notice] = "Article was created successfully."
+     redirect_to @article 
+   else
+     render 'new'
+   end 
   end
   
 end
